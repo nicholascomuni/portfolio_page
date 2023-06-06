3207accb8a5282f3a8b1138829fba1f57c1654f7
Nicholas Comuni <nick.comuni@outlook.com>
2023-06-06 20:17:19 -0300

    Added social media info

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const buttonDropdown = document.querySelector(".button_header");
+const menuDropdown = document.querySelector(".menu_header");
+buttonDropdown.addEventListener("click", ()=> menuDropdown.classList.toggle("active"));
