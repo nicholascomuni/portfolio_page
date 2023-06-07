10914d1edbab3d86ada9540abb9644f1b8f1255c
Nicholas Comuni <nick.comuni@outlook.com>
2023-06-06 23:31:58 -0300

    Changed a lot of things

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 const buttonDropdown = document.querySelector(".button_header");
 const menuDropdown = document.querySelector(".menu_header");
+
 buttonDropdown.addEventListener("click", ()=> menuDropdown.classList.toggle("active"));
