164a7c2bb9de390ed20a2f0ce2f0a1e97f2ddd47
Nicholas Comuni <nick.comuni@outlook.com>
2023-06-11 23:45:53 -0300

    Changed timeline

--- js/script.js
+++ js/script.js
@@ -2,3 +2,30 @@ const buttonDropdown = document.querySelector(".button_header");
 const menuDropdown = document.querySelector(".menu_header");
 
 buttonDropdown.addEventListener("click", ()=> menuDropdown.classList.toggle("active"));
+
+function copyToClipboard(email) {
+    const el = document.createElement('textarea');
+    el.value = email;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+
+    // var tooltip = document.getElementById("tooltip");
+    // tooltip.style.display = "block";
+
+    // setTimeout(function() {
+    //     tooltip.style.display = "none";
+    //   }, 1000);
+
+    var tooltip = document.getElementById("tooltip");
+    tooltip.classList.add("show");
+    
+    setTimeout(function() {
+        tooltip.classList.remove("show");
+    }, 1000);
+  }
+  
+  
+  
+ 
